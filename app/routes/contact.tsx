e23d31c6fb675657f2af1a5bf4566c5311b97c6b
enyichiaagu <enyichiaagu@gmail.com>
2025-03-19 22:08:47 +0100

    Finished markup for contact route

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -10,13 +10,31 @@ export const Route = createFileRoute('/contact')({
 function Contact() {
   return (
     <>
-      <p>Contact Me</p>
-      <form action='' method='post'>
-        <label htmlFor=''>Email</label>
-        <input type='email' name='' id='' />
-        <label htmlFor=''>Message</label>
-        <textarea name='' id='' placeholder='Write me a message'></textarea>
-        <button>Send</button>
+      <p className='text-2xl'>Contact Me</p>
+      <form action='' method='post' className='mt-5'>
+        <div className='mb-2'>
+          <label htmlFor='email'>Email</label>
+          <br />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            className='border border-gray-400 w-md px-3 py-1.5'
+          />
+        </div>
+        <div className='mb-2'>
+          <label htmlFor='message'>Message</label>
+          <br />
+          <textarea
+            name='message'
+            id='message'
+            placeholder='Write me a message'
+            className='border border-gray-400 w-md px-3 py-1.5 h-50'
+          ></textarea>
+        </div>
+        <button className='bg-blue-900 text-gray-50 px-4 py-2 rounded'>
+          Send
+        </button>
       </form>
     </>
   );
